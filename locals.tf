41104bb5cee88c8992accb4e01c05a480837bb83
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-12 16:26:53 +0100

    terraform

--- locals.tf
+++ locals.tf
@@ -8,10 +8,10 @@ locals {
   }
 
   all_tags = [
-    {key = "env", value = var.env}
-    {key = "project", value = "roboshop"}
-    {key = "business_unit", value = "ecommerce"}
-    {key = "owner", value = "ecommerce-project"}
+    { key = "env", value = var.env},
+    {key = "project", value = "roboshop"},
+    {key = "business_unit", value = "ecommerce"},
+    {key = "owner", value = "ecommerce-project"},
     {key = "Name", value = "${var.env}-${var.component}"}
   ]
 }
