e2185a7630abef54e8fa24c555d788be88e3f592
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-17 16:38:04 +0100

    terraform

--- user-data.sh
+++ user-data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo labauto anisble
+labauto anisble
 ansible-pull -i localhost, -U https://github.com/KarthikPalakurthy/ansible-roboshop roboshop.yml -e ROLE_NAME=${component} -e env=${env} | tee /opt/ansible.log
