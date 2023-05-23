44f0438e3c99631d0055a3d8539750bd0239e59f
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-23 20:21:53 +0100

    terraform

--- main.tf
+++ main.tf
@@ -18,13 +18,13 @@ resource "aws_security_group" "app" {
     protocol         = "tcp"
     cidr_blocks      = var.bastion_cidr
   }
-  ingress {
-    description      = "PROMETHEUS"
-    from_port        = 9100
-    to_port          = 9100
-    protocol         = "tcp"
-    cidr_blocks      = var.monitor_cidr
-  }
+#  ingress {
+#    description      = "PROMETHEUS"
+#    from_port        = 9100
+#    to_port          = 9100
+#    protocol         = "tcp"
+#    cidr_blocks      = var.monitor_cidr
+#  }
 
   egress {
     from_port        = 0
